bb5db8b6e954d3dc0cbcd4a839ded497b1397251
Giuseppe Silletti <giuseppesilletti@kinsta.com>
2023-06-29 11:34:22 +0200

    WIP

---
+++ server.js
@@ -0,0 +1,24 @@
+// Import required modules
+const express = require('express');
+
+// Create an Express application
+const app = express();
+
+// Define a single endpoint
+app.get('/increase-cpu-load', (req, res) => {
+  // Start an infinite loop to keep the CPU load high
+  while (true) {
+    // Perform a CPU-intensive task
+    const iterations = 1000000000; // Increase this number to increase the CPU load
+    let result = 0;
+    for (let i = 0; i < iterations; i++) {
+      result += Math.sqrt(i);
+    }
+  }
+});
+
+// Start the server
+const port = 5001;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
